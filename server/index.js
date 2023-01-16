b2facd404a0e898d66d0ff1c95b96aad3ed78191
V ANIRRUTH <74400297+anirruth0312@users.noreply.github.com>
2023-01-16 23:08:55 +0530

    Added server index

---
+++ server/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+app.use(cors());
+
+//creating server
+const server = http.createServer(app);
+
+//io method
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected with id : ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  //sending event
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected with id :", socket.id);
+  });
+});
+
+//3001 as 3000 is for react app
+server.listen(3001, () => {
+  console.log("SERVER IS UP AND RUNNING ");
+});
